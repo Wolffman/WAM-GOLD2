fae29278c7a6ed57d038531740b7611263fb3f24
samuel_wolff <samuelwolff450@gmail.com>
2017-03-16 07:46:38 -0400

    Added 100 dinosaurs

--- WAM/src/Main.java
+++ WAM/src/Main.java
@@ -5,11 +5,15 @@ import javax.swing.*;
  */
 public class Main extends JPanel{
     public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
-    private int one=1;
-//adsfaolhf
+    private Timer timer;
+    private String key= " ";
+    private String keyType="None";
+    private boolean menu;
+    public Main(){
 
+
+    }
     public static void main(String[] args) {
-        System.out.println("yee!!!");
         JFrame window = new JFrame("Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 20 due to title bar.
